3e444c35cd644df8d9852f52118aee9ed0489a7e
citraulia <citraaulia1812@gmail.com>
2024-11-10 19:30:44 +0700

    lanjutin dari 1:17:50

---
+++ home.php
@@ -0,0 +1,114 @@
+<?php
+    include 'components/connestion.php';
+?>
+<style type="text/css">
+    <?php include 'style.css'; ?>
+</style>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet">
+    <title>Kopiko - home page</title>
+</head>
+<body>
+    <?php include 'components/header.php'; ?>
+    <div class="main">
+        <?php include 'components/footer.php'; ?>
+        <section class="home-section">
+            <div class="slider">
+                <div class="slider__slider slide1">
+                    <div class="overlay"></div>
+                    <div class="slide-detail">
+                        <h1>wfeh djifie djiedn jf</h1>
+                        <p>deih fefiefb iwdiwdi iwdoqdo idiebfe iwdowbf wodowf iwfob</p>
+                        <a href="view_products.php" class="btn"> shop now</a>
+                    </div>
+                    <div class="hero-dec-top"></div>
+                    <div class="hero-dec-bottom"></div>
+                </div>
+                <!-- slide end -->
+                <div class="slider__slider slide2">
+                    <div class="overlay"></div>
+                    <div class="slide-detail">
+                        <h1>welcome to my shop</h1>
+                        <p>deih fefiefb iwdiwdi iwdoqdo idiebfe iwdowbf wodowf iwfob</p>
+                        <a href="view_products.php" class="btn"> shop now</a>
+                    </div>
+                    <div class="hero-dec-top"></div>
+                    <div class="hero-dec-bottom"></div>
+                </div>
+                <!-- slide end -->
+                <div class="slider__slider slide3">
+                    <div class="overlay"></div>
+                    <div class="slide-detail">
+                        <h1>wfeh djifie djiedn jf</h1>
+                        <p>deih fefiefb iwdiwdi iwdoqdo idiebfe iwdowbf wodowf iwfob</p>
+                        <a href="view_products.php" class="btn"> shop now</a>
+                    </div>
+                    <div class="hero-dec-top"></div>
+                    <div class="hero-dec-bottom"></div>
+                </div>
+                <!-- slide end -->
+                <div class="slider__slider slide4">
+                    <div class="overlay"></div>
+                    <div class="slide-detail">
+                        <h1>wfeh djifie djiedn jf</h1>
+                        <p>deih fefiefb iwdiwdi iwdoqdo idiebfe iwdowbf wodowf iwfob</p>
+                        <a href="view_products.php" class="btn"> shop now</a>
+                    </div>
+                    <div class="hero-dec-top"></div>
+                    <div class="hero-dec-bottom"></div>
+                </div>
+                <!-- slide end -->
+                <div class="slider__slider slide5">
+                    <div class="overlay"></div>
+                    <div class="slide-detail">
+                        <h1>wfeh djifie djiedn jf</h1>
+                        <p>deih fefiefb iwdiwdi iwdoqdo idiebfe iwdowbf wodowf iwfob</p>
+                        <a href="view_products.php" class="btn"> shop now</a>
+                    </div>
+                    <div class="hero-dec-top"></div>
+                    <div class="hero-dec-bottom"></div>
+                </div>
+                <!-- slide end -->
+                <div class="left-arrow"><i class="bx bxs-left-arrow"></i></div>
+                <div class="left-arrow"><i class="bx bxs-right-arrow"></i></div>
+            </div>
+        </section>
+        <!-- home slider end -->
+        <section class="thumb">
+            <div class="box-container">
+                <div class="box">
+                    <img src="img/thumb2.jpg">
+                    <h3>green tea</h3>
+                    <p>deih fefiefb iwdiwdi iwdoqdo idiebfe iwdowbf wodowf iwfob</p>
+                    <i class="bx bx-chevron-right"></i>
+                </div>
+                <div class="box">
+                    <img src="img/thumb0.jpg">
+                    <h3>lemon tea</h3>
+                    <p>deih fefiefb iwdiwdi iwdoqdo idiebfe iwdowbf wodowf iwfob</p>
+                    <i class="bx bx-chevron-right"></i>
+                </div>
+                <div class="box">
+                    <img src="img/thumb1.jpg">
+                    <h3>green coffee</h3>
+                    <p>deih fefiefb iwdiwdi iwdoqdo idiebfe iwdowbf wodowf iwfob</p>
+                    <i class="bx bx-chevron-right"></i>
+                </div>
+                <div class="box">
+                    <img src="img/thumb.jpg">
+                    <h3>green tea</h3>
+                    <p>deih fefiefb iwdiwdi iwdoqdo idiebfe iwdowbf wodowf iwfob</p>
+                    <i class="bx bx-chevron-right"></i>
+                </div>
+            </div>
+        </section>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+    <script src="script.js"></script>
+    <?php include 'components/alert.php'; ?>
+</body>
+</html>
